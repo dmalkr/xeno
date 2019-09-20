a664a0a4306f7ef4ec27123ae4c9edf807e10d6c
Dmitry Krylov <dima@krylov.ws>
2019-09-20 21:07:15 +1000

    WIP: More robust benchmark

--- app/Bench.hs
+++ app/Bench.hs
@@ -1,23 +1,69 @@
-import qualified Data.ByteString as BS
-import System.IO.Posix.MMap
-import Xeno.SAX
+{-# LANGUAGE QuasiQuotes #-}
+import Control.Monad
 import Data.Time.Clock
+import System.IO.Posix.MMap
+import Text.InterpolatedString.Perl6 (qc)
+import Xeno.SAX
+import qualified Data.ByteString as BS
+
 
 main :: IO ()
 main = do
-    -- let fn = "data/1htq.xml"
-    let fn = "data/enwiki-20190901-pages-logging1.xml"
-    -- let fn = "data/enwiki-20190901-pages-meta-current24.xml-p30503451p32003451.xml"
-    putStrLn fn
-    bs <- unsafeMMapFile fn
-    -- bs <- BS.readFile fn
-    print (BS.length bs)
+    let prefix = "/i/p/migamake/xeno/data/"
+        files = map (prefix ++)
+                [ {- 921 Mb -} "1htq.xml"
+                , {- 190 Mb -} "enwiki-20190901-abstract10.xml"
+                , {- 1.6 Gb -} "enwiki-20190901-pages-logging1.xml"
+                , {- 4.0 Gb -} "enwiki-20190901-pages-meta-current24.xml-p30503451p32003451.xml"
+                -- , {-  21 Gb -} "enwiki-20190901-pages-meta-history2.xml"
+                --
+                --
+                , {- 921 Mb -} "1htq.xml"
+                , {- 190 Mb -} "enwiki-20190901-abstract10.xml"
+                , {- 1.6 Gb -} "enwiki-20190901-pages-logging1.xml"
+                , {- 4.0 Gb -} "enwiki-20190901-pages-meta-current24.xml-p30503451p32003451.xml"
+
+                , {- 921 Mb -} "1htq.xml"
+                , {- 190 Mb -} "enwiki-20190901-abstract10.xml"
+                , {- 1.6 Gb -} "enwiki-20190901-pages-logging1.xml"
+                , {- 4.0 Gb -} "enwiki-20190901-pages-meta-current24.xml-p30503451p32003451.xml"
+
+                , {- 921 Mb -} "1htq.xml"
+                , {- 190 Mb -} "enwiki-20190901-abstract10.xml"
+                , {- 1.6 Gb -} "enwiki-20190901-pages-logging1.xml"
+                , {- 4.0 Gb -} "enwiki-20190901-pages-meta-current24.xml-p30503451p32003451.xml"
+
+                , {- 921 Mb -} "1htq.xml"
+                , {- 190 Mb -} "enwiki-20190901-abstract10.xml"
+                , {- 1.6 Gb -} "enwiki-20190901-pages-logging1.xml"
+                , {- 4.0 Gb -} "enwiki-20190901-pages-meta-current24.xml-p30503451p32003451.xml"
+
+                , {- 921 Mb -} "1htq.xml"
+                , {- 190 Mb -} "enwiki-20190901-abstract10.xml"
+                , {- 1.6 Gb -} "enwiki-20190901-pages-logging1.xml"
+                , {- 4.0 Gb -} "enwiki-20190901-pages-meta-current24.xml-p30503451p32003451.xml"
+                ]
+        --files = map (prefix ++)
+        --        [ [> 4.0 Gb <] "enwiki-20190901-pages-meta-current24.xml-p30503451p32003451.xml"
+        --        ]
     --
-    -- print $ fold (\m _ -> m + 1) (\m _ _ -> m) const const const const 0 bs
+    deltas <- forM files $ \fn -> do
+        putStrLn [qc|Processing file '{fn}'|]
+        --
+        -- NOTE: It is need to cache file in memory BEFORE start test.
+        --       It can be done with `vmtouch` utility for example (`vmtouch -vtL *`).
+        --
+        bs <- unsafeMMapFile fn
+        -- bs <- BS.readFile fn
+        putStrLn [qc|  size: {BS.length bs `div` (1024*1024)} Mb|]
+        -- print $ fold (\m _ -> m + 1) (\m _ _ -> m) const const const const 0 bs
+        start <- getCurrentTime
+        let res = validate bs
+        putStrLn [qc|  process result: {res}|]
+        finish <- getCurrentTime
+        let delta = finish `diffUTCTime` start
+        putStrLn [qc|  processing time: {delta}|]
+        return delta
     --
-    start <- getCurrentTime
-    let res = validate bs
-    print res
-    finish <- getCurrentTime
-    let delta = finish `diffUTCTime` start
-    print delta
+    putStrLn "------"
+    putStrLn [qc|Total: {sum deltas}|]
