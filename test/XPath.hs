b289193604f59b17bbeafd5ec88d216818a45a0d
Michal J. Gajda <mjgajda@migamake.com>
2019-11-05 08:40:13 +0100

    XPath planning.

---
+++ test/XPath.hs
@@ -0,0 +1,31 @@
+-- | This module sketches tests
+--   intended for adding streaming and XPath function to `xeno`.
+module XPathSpec where
+
+-- | Extract DOM for each <b/> element separately. Ignore the others.
+
+xmlAB = "<document><b></b><a/><b></b><c/></document>"
+
+-- | Template for `Process` override
+ignoreProcess :: Process
+ignoreProcess  = undefined -- process that ignores all elements
+
+-- | Implement DOM parsing as Process
+domProcess :: Process
+domProcess  = undefined
+
+processB = ignoreProcess {
+             startElement eltName | eltName == "b" =
+               writeIORef domResult $ Just domResult
+               continueWith domProcess
+           , endElement eltName = do
+               yield domResult
+               writeIORef domResult Nothing
+           }
+
+-- TODO:
+-- -- continueWith
+-- yield
+--
+
+
